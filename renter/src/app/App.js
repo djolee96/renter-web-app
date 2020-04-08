12a4b2e229b61613cd39084a29df583127d620b6
unknown <milan.491989@yahoo.com>
2020-04-08 16:01:06 +0200

    folder structure done

---
+++ renter/src/app/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../scss/app.css';
+
+class App extends React.Component {
+  render() {
+    return(
+    <div className="App">
+
+    </div>)
+  };
+}
+
+export default App;
