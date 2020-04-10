503ac90881c681845fc80539362226572199fbb3
unknown <milan.491989@yahoo.com>
2020-04-11 01:58:17 +0200

    statistic page/ card concept

--- renter/src/app/components/Main.jsx
+++ renter/src/app/components/Main.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Dashboard from './main/Dashboard'
-
+import StatisticPage from './main/StatisticPage'
 
 class Main extends React.Component{
     render(){
         return(
         <div className="main">
             <Navbar/>
-            <Dashboard/>
+            {/* <Dashboard/> */}
+            <StatisticPage/>
         </div>
         )   
  }
---
+++ renter/src/app/components/main/StatisticPage.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+
+
+class StatisticPage extends React.Component{
+    render(){
+        return(
+        <div className="statistic-page">
+            <div className="statistic-property-card">
+                <div className="statistic-property-card-image"> </div>
+                <div className="statistic-property-card-info"> 
+                    <h1>Property Name</h1>
+                    <h4>Value: 10.000</h4>
+                    <h4>Income: 1.200</h4>
+                    <h4>Expense: 900</h4>
+                </div>
+                <div className="statistic-property-card-roi"> <h4>ROI: 10%</h4> </div>
+            </div>
+            <div className="statistic-property-card">
+                <div className="statistic-property-card-image"> </div>
+                <div className="statistic-property-card-info"> 
+                    <h1>Property Name</h1>
+                    <h4>Value: 10.000</h4>
+                    <h4>Income: 1.200</h4>
+                    <h4>Expense: 900</h4>
+                </div>
+                <div className="statistic-property-card-roi"> <h4>ROI: 10%</h4> </div>
+            </div>
+            <div className="statistic-property-card">
+                <div className="statistic-property-card-image"> </div>
+                <div className="statistic-property-card-info"> 
+                    <h1>Property Name</h1>
+                    <h4>Value: 10.000</h4>
+                    <h4>Income: 1.200</h4>
+                    <h4>Expense: 900</h4>
+                </div>
+                <div className="statistic-property-card-roi"> <h4>ROI: 10%</h4> </div>
+            </div>
+        </div>
+        )   
+ }
+
+}
+
+export default StatisticPage
